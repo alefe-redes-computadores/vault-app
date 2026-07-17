cd816057940c76d3403368005b2cbf0f55dbbbcd
alefe-redes-computadores <202509217108@alunos.estacio.br>
2026-07-17 08:46:23 -0300

    Criar layout.tsx

---
+++ app/saude/medicamentos/[id]/layout.tsx
@@ -0,0 +1,11 @@
+export async function generateStaticParams() {
+  return [];
+}
+
+export default function MedicamentoLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
