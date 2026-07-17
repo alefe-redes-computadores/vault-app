7a1176de313f720f77db4a90c112621fa72d92cc
alefe-redes-computadores <202509217108@alunos.estacio.br>
2026-07-17 08:45:42 -0300

    Atualizar o layout.tsx

--- app/categoria/[id]/layout.tsx
+++ app/categoria/[id]/layout.tsx
@@ -1,10 +1,8 @@
-// Esta função gera as páginas estáticas (executada no servidor)
-// Retorna um array vazio porque os dados são carregados no cliente
 export async function generateStaticParams() {
   return [];
 }
 
-export default function EditLayout({
+export default function CategoryLayout({
   children,
 }: {
   children: React.ReactNode;
