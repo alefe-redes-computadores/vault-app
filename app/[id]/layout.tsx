df9377c8b1e40aa2c3542daefae632e41c1e5365
alefe-redes-computadores <202509217108@alunos.estacio.br>
2026-07-17 08:44:45 -0300

    Atualizar o layout.tsx

--- app/[id]/layout.tsx
+++ app/[id]/layout.tsx
@@ -1,6 +1,4 @@
-// Esta função gera as páginas estáticas (executada no servidor)
 export async function generateStaticParams() {
-  // Retorna um array vazio porque os dados são carregados no cliente
   return [];
 }
 
