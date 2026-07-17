a8b25dc3397313cec9d929d127ac08461673ae1e
alefe-redes-computadores <202509217108@alunos.estacio.br>
2026-07-16 23:40:04 -0300

    Atualizar o layout.tsx

--- app/categoria/[id]/layout.tsx
+++ app/categoria/[id]/layout.tsx
@@ -1,13 +1,11 @@
 import { CATEGORIES } from "@/lib/types";
 
-// Esta função gera as páginas estáticas (executada no servidor)
 export async function generateStaticParams() {
   return Object.keys(CATEGORIES).map((id) => ({
     id: id,
   }));
 }
 
-// Layout simples que renderiza o children
 export default function CategoryLayout({
   children,
 }: {
