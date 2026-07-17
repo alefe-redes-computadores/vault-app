a57ff31af354fb5b2dd02278ff4425ae24cacc6a
alefe-redes-computadores <202509217108@alunos.estacio.br>
2026-07-17 08:43:43 -0300

    Atualizar o next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "export", // REMOVA ESTA LINHA
+  output: "export", // ← DESCOMENTADO
   images: { unoptimized: true },
   reactStrictMode: true,
 };
